3dece5ec26fed30ec649fb21b099c76905a0a4c6
oakley-martin <159910996+oakley-martin@users.noreply.github.com>
2026-05-05 11:37:50 -0400

    Update diff_preproc_c2.py

--- diff_preproc_c2.py
+++ diff_preproc_c2.py
@@ -1,9 +1,9 @@
 # Copyright 2026 Yixin Ma, Oakley Martin, Hong-Hsi Lee, and Susie Huang (MGH/Martinos Center)
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
-
+#
+# With questions or issues, contact Oakley Martin at omartin3@mgh.harvard.edu
 # Most recent update: 5 May 2026
 
 import sys
